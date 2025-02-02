91df5c80df51c3f29de7033b9fc750dcffdd1f4a
Trukhanov Danylo <trukhanov.danya@gmail.com>
2025-02-02 12:47:51 +0200

    Create worker and position models
    Create worker model subclassing AbstractUser, add position field.
    Create position model with name field.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.SET_NULL, related_name="workers")
