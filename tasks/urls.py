e3b026c0364f0ef428830e53b4c4d5d8cedeb6ec
Trukhanov Danylo <trukhanov.danya@gmail.com>
2025-02-03 16:46:39 +0200

    Add task-list path, create list view for task model

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,12 @@
 
 from django.urls import path, include
 
-from tasks.views import index
+from tasks.views import index, TaskListView
 
 urlpatterns = [
     path("", index, name="home"),
+    path("tasks/", TaskListView.as_view(), name="task-list"),
+
 ]
 
 app_name = "tasks"
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.views import generic
+
+from tasks.models import Task
+from users.models import Worker
 
 
 def index(request):
     return render(request, "base.html")
+
+
+class TaskListView(generic.ListView):
+    model = Task
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["current_time"] = timezone.now()
+        return context
+
+
+class WorkerListView(generic.ListView):
+    model = Worker
