40b54943e3150958208177d2332fb029b15a0b5e
Trukhanov Danylo <trukhanov.danya@gmail.com>
2025-02-04 12:35:05 +0200

    Change creation form fields to make it look better

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,6 +1,11 @@
-from django import forms
+import datetime
 
-from tasks.models import Task
+from django import forms
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+
+from tasks.models import Task, TaskType
+from users.models import Worker
 
 
 class TaskCreationForm(forms.ModelForm):
@@ -14,6 +19,38 @@ class TaskCreationForm(forms.ModelForm):
             }
         ),
     )
+
+    deadline = forms.DateField(
+        widget=forms.DateInput(
+            attrs={
+                "class": "form-control",
+                "type": "date",
+            }
+        )
+    )
+    is_completed = forms.BooleanField(
+        widget = forms.CheckboxInput(
+            attrs={
+                "class": "form-check-input",
+            }
+        )
+    )
+
+    task_type = forms.ModelChoiceField(
+        queryset=TaskType.objects.all(),
+        widget=forms.RadioSelect
+    )
+
+    priority = forms.ChoiceField(
+        choices=Task.PriorityChoice,
+        widget=forms.RadioSelect
+    )
+
+    assignees = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
     class Meta:
         model = Task
         fields = "__all__"
