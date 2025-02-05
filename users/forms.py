d67a8e29df071fa9689eb9fa33f5494774552f5a
Trukhanov Danylo <trukhanov.danya@gmail.com>
2025-02-05 17:06:23 +0200

    Create tests for views

--- users/forms.py
+++ users/forms.py
@@ -28,4 +28,4 @@ class WorkerUpdateForm(forms.ModelForm):
 
     class Meta:
         model = Worker
-        fields = ("first_name", "last_name", "email", "position")
+        fields = ("username", "first_name", "last_name", "email", "position")
